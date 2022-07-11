8ead3df4f19f7f2b37f93eaa956b058ca8d51ee1
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-11 15:21:18 +0100

    Section(api basics) - 10: reading the data from the database in the api

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,24 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductsController: ControllerBase
+    public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProducts()
+        private readonly StoreContext _storeContext;
+
+        public ProductsController(StoreContext storeContext)
         {
-            return "This will be a list of products";
+            this._storeContext = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _storeContext.Products.ToListAsync();
+            return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public string GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return "This will be a single product :" + id;
+            var product = await _storeContext.Products.FindAsync(id);
+            return Ok(product);
         }
-        
+
     }
 }

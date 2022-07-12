c62beb8b33c6e2026fa43b5cd11010b7bb0053ff
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-12 09:58:56 +0100

    Section(api architecture) - 13: adding a repository and interface

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -30,8 +31,9 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             string mySqlConnectionStr = _configuration.GetConnectionString("DefaultConnection");
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
-            services.AddDbContext<StoreContext>(x=>x.UseMySQL(mySqlConnectionStr));
+            services.AddDbContext<StoreContext>(x => x.UseMySQL(mySqlConnectionStr));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+        Task<Product> GetProductByIdAsync(int id);
+    }
+}

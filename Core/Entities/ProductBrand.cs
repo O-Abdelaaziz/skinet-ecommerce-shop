ab9128cf1a2ce4ca5630484681b127f67c189bb2
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-12 10:36:47 +0100

    Section(api architecture) - 16: creating a new migration for the entities

--- Core/Entities/ProductBrand.cs
+++ Core/Entities/ProductBrand.cs
@@ -2,7 +2,7 @@
 {
     public class ProductBrand : BaseEntity
     {
-        public string name { get; set; }
-        public string description { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }

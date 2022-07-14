c78a7a3366a3c4efab0065c97e6978f3dd9c2e8d
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-14 10:55:27 +0100

    Section(api error handling) - 33: creating a test controller for errors

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,51 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _storeContext;
+
+        public BuggyController(StoreContext storeContext)
+        {
+            this._storeContext = storeContext;
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var product = this._storeContext.Products.Find(50);
+            if (product != null)
+            {
+                return Ok(product);
+            }
+            return NotFound("no Product found with provided id: " + 50);
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerErrorRequest()
+        {
+            var product = this._storeContext.Products.Find(50); // this is null
+            var productToString = product.ToString(); // this make server error bcs product is null
+
+            return Ok();
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+
+        [HttpGet("bad-request/{id}")]
+        public ActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+        }
+    }
+}
--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -10,9 +10,8 @@ using AutoMapper;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+
+    public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product> _productRepository;
         private readonly IGenericRepository<ProductBrand> _productBrandRepository;

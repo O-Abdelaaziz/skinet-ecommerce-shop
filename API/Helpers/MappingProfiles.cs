15906443ffd07200cfa5f126172e2ba5c73b9df6
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-14 09:52:23 +0100

    Section(api generic repository) - 31: adding a custom value resolver for automapper

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -12,7 +12,8 @@ namespace API.Helpers
                 //d => destination
                 //s => source
                 .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
-                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductPictureUrlResolver>());
         }
     }
 }
---
+++ API/Helpers/ProductPictureUrlResolver.cs
@@ -0,0 +1,27 @@
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Helpers
+{
+    public class ProductPictureUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
+    {
+        private readonly IConfiguration _configuration;
+        private static readonly string API_URL = "ApiUrl";
+
+        public ProductPictureUrlResolver(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return this._configuration[API_URL] + source.PictureUrl;
+            }
+            return null;
+        }
+    }
+}

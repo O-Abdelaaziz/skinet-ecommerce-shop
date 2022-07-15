c4ac27d08b16df0b9370e5e00fa0602692c90c39
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-15 11:57:18 +0100

    Section(api paging and searching) - 41: adding a sorting specification class (2)

--- Core/Specifications/ProductsWithBrandsAndTypesSpecification.cs
+++ Core/Specifications/ProductsWithBrandsAndTypesSpecification.cs
@@ -4,10 +4,27 @@ namespace Core.Specifications
 {
     public class ProductsWithBrandsAndTypesSpecification : BaseSpecification<Product>
     {
-        public ProductsWithBrandsAndTypesSpecification()
+        public ProductsWithBrandsAndTypesSpecification(string sort)
         {
             AddInclude(x => x.ProductBrand);
             AddInclude(x => x.ProductType);
+            AddOrderBy(x => x.Name);
+
+            if (!string.IsNullOrEmpty(sort))
+            {
+                switch (sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    default:
+                        AddOrderBy(n => n.Name);
+                        break;
+                }
+            }
         }
 
         public ProductsWithBrandsAndTypesSpecification(int id) : base(x => x.Id == id)

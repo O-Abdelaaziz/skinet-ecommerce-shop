a50733131ebb6d91b44b157972cabbf38ff4a627
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-16 12:07:15 +0100

    Section(client angular setup) - 52: adding a nav bar component

---
+++ client/src/app/layouts/main-navbar/main-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-navbar',
+  templateUrl: './main-navbar.component.html',
+  styleUrls: ['./main-navbar.component.scss']
+})
+export class MainNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

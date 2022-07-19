c8d8bc5427bc70faf6f30ba51738f2c531fef81d
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-19 20:49:13 +0100

    Section(api basket) - 97: implementing the basket repository

--- Infrastructure/Data/BasketRepository.cs
+++ Infrastructure/Data/BasketRepository.cs
@@ -1,25 +1,42 @@
 ﻿using Core.Entities;
 using Core.Interfaces;
+using StackExchange.Redis;
 using System;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Data
 {
     public class BasketRepository : IBasketRepository
     {
-        public Task<bool> DeleteBasketAsync(string basketId)
+        private readonly IDatabase _database;
+
+        public BasketRepository(IConnectionMultiplexer redis)
         {
-            throw new NotImplementedException();
+            this._database = redis.GetDatabase();
         }
 
-        public Task<CustomerBasket> GetBasketAsync(string basketId)
+        public async Task<CustomerBasket> GetBasketAsync(string basketId)
         {
-            throw new NotImplementedException();
+            var data = await _database.StringGetAsync(basketId);
+
+            return !data.IsNullOrEmpty ? JsonSerializer.Deserialize<CustomerBasket>(data) : null;
         }
 
-        public Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+        public async Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
         {
-            throw new NotImplementedException();
+            var created = await _database.StringSetAsync(basket.Id, JsonSerializer.Serialize(basket), TimeSpan.FromDays(30));
+
+            if (!created) return null;
+
+            return await GetBasketAsync(basket.Id);
+        }
+
+        public async Task<bool> DeleteBasketAsync(string basketId)
+        {
+            return await _database.KeyDeleteAsync(basketId);
         }
     }
+
 }
+

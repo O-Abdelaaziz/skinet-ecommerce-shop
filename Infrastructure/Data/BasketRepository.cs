1e592428f997d1c2d1afc604b5b6b6b2f04ec84b
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-19 20:28:55 +0100

    Section(api basket) - 96: creating a basket repository interface

---
+++ Infrastructure/Data/BasketRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class BasketRepository : IBasketRepository
+    {
+        public Task<bool> DeleteBasketAsync(string basketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomerBasket> GetBasketAsync(string basketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

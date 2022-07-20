3d7b0c737e0f607689db036f1abadc606d5a02e0
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-20 09:33:01 +0100

    Section(client basket) - 101: basket service methods

--- client/src/app/basket/services/basket.service.ts
+++ client/src/app/basket/services/basket.service.ts
@@ -1,9 +1,41 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {BehaviorSubject, Observable} from "rxjs";
+import {map} from "rxjs/operators";
+import {IBasket} from "../../shared/models/basket";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasketService {
+  public readonly baseUrl: string = environment.baseUrl;
+  private basketSource: BehaviorSubject<IBasket> = new BehaviorSubject<IBasket>({id: '', items: []});
+  public basket$ = this.basketSource.asObservable();
 
-  constructor() { }
+  constructor(private _httpClient: HttpClient) {
+  }
+
+  public getBasket(id: string) {
+    return this._httpClient.get<IBasket>(`${this.baseUrl}/basket?id=${id}`)
+      .pipe(
+        map((basket: IBasket) => {
+          this.basketSource.next(basket);
+        })
+      );
+  }
+
+  public setBasket(basket: IBasket) {
+    return this._httpClient.post<IBasket>(`${this.baseUrl}/basket`, basket)
+      .subscribe(
+        (response: IBasket) => {
+          this.basketSource.next(response)
+        }, (error) => {
+          console.log("Error In setBasket : " + error)
+        });
+  }
+
+  public getCurrentBasket() {
+    return this.basketSource.value;
+  }
 }

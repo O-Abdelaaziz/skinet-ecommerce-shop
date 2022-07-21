a54c815d17552f8188a95d9b0ad351f7056c9dba
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-21 13:56:34 +0100

    Section(client basket) - 112: adding the checkout module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,7 +7,16 @@ import {NotFoundComponent} from "./core/pages/errors/not-found/not-found.compone
 const routes: Routes = [
   {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(s => s.ShopModule), data: {breadcrumb: 'Shop'}},
-  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(b => b.BasketModule), data: {breadcrumb: 'Basket'}},
+  {
+    path: 'basket',
+    loadChildren: () => import('./basket/basket.module').then(b => b.BasketModule),
+    data: {breadcrumb: 'Basket'}
+  },
+  {
+    path: 'checkout',
+    loadChildren: () => import('./checkout/checkout.module').then(c => c.CheckoutModule),
+    data: {breadcrumb: 'Checkout'}
+  },
   {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
   {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
   {path: '**', redirectTo: '/not-found', pathMatch: 'full'},

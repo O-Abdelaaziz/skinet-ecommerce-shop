8e908bf02b54ab2c1f4871175765708f1234eeca
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-22 10:04:11 +0100

    Section(api identity) - 117: seeding identity data

---
+++ Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUsersAsync(UserManager<AppUser> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var user = new AppUser
+                {
+                    DisplayName = "Ouakala Abdelaaziz",
+                    Email = "ouakala.abdelaaziz@test.dz",
+                    UserName = "ouakala.abdelaaziz@test.dz",
+                    Address =
+                    {
+                        FirstName="Ouakala",
+                        LastName="Abdelaaziz",
+                        Country="Algeria",
+                        City="Algeria",
+                        State="Algeria",
+                        Street="Algeria",
+                        ZipCode="02000"
+                    }
+                };
+                await userManager.CreateAsync(user, "#Test01234560");
+            }
+        }
+    }
+}

cb98d16f77072e16c2b3e1e9cc72d6f8b3cd064d
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-23 17:33:58 +0100

    Section(api identity) - 121: adding a token generation service

---
+++ Infrastructure/services/TokenService.cs
@@ -0,0 +1,49 @@
+﻿using Core.Entities.Identity;
+using Core.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Infrastructure.services
+{
+    class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+            this._key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this._configuration["Token:Key"]));
+        }
+
+        public string CreateToken(AppUser appUser)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, appUser.Email),
+                new Claim(ClaimTypes.GivenName, appUser.DisplayName)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds,
+                Issuer = _configuration["Token:Issuer"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}

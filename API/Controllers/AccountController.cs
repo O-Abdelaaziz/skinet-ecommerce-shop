a749a49d8fbd59887a43186d6940dc1cc8832584
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-23 13:56:23 +0100

    Section(api identity) - 119: adding an account controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using API.Dtos;
+using API.Errors;
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized(new ApiResponse(401, HttpStatusCode.Unauthorized.ToString())));
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded) return Unauthorized(new ApiResponse(401, HttpStatusCode.Unauthorized.ToString())));
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = "This can be agenerated token",
+                DisplayName = user.DisplayName
+            };
+        }
+    }
+}

977b6206f38fce1d76ccb971d9ae5c053d85ef8d
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-24 13:06:31 +0100

    Section(api validation) - 129: updating swagger config for identity

--- API/Extensions/SwaggerServiceExtention.cs
+++ API/Extensions/SwaggerServiceExtention.cs
@@ -28,6 +28,24 @@ namespace API.Extensions
                         Url = new Uri("http://ouakala-abdelaaziz.epizy.com/")
                     }
                 });
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement { { securitySchema, new[] { "Bearer" } } };
+                c.AddSecurityRequirement(securityRequirement);
             });
 
             return services;

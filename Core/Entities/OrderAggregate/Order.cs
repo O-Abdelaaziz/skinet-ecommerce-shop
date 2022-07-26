8bc5b81d20e584ede61a32864039cdb94f2075b8
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-26 22:11:53 +0100

    Section(api orders) - 153: creating an order service

--- Core/Entities/OrderAggregate/Order.cs
+++ Core/Entities/OrderAggregate/Order.cs
@@ -9,14 +9,13 @@ namespace Core.Entities.OrderAggregate
         {
         }
 
-        public Order(IReadOnlyList<OrderItem> orderItems, string buyerEmail, Address shipToAddress, DeliveryMethod deliveryMethod, decimal subtotal, string paymentIntentId)
+        public Order(IReadOnlyList<OrderItem> orderItems, string buyerEmail, Address shipToAddress, DeliveryMethod deliveryMethod, decimal subtotal)
         {
             BuyerEmail = buyerEmail;
             ShipToAddress = shipToAddress;
             DeliveryMethod = deliveryMethod;
             OrderItems = orderItems;
             Subtotal = subtotal;
-            //PaymentIntentId = paymentIntentId;
         }
 
         public string BuyerEmail { get; set; }
---
+++ Infrastructure/services/OrderService.cs
@@ -0,0 +1,64 @@
+﻿using Core.Entities;
+using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infrastructure.services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IGenericRepository<Order> _orderRepository;
+        private readonly IGenericRepository<DeliveryMethod> _deliveryMethodRepository;
+        private readonly IGenericRepository<Product> _productRepository;
+        private readonly IBasketRepository _basketRepository;
+
+        public OrderService(IGenericRepository<Order> orderRepository, IGenericRepository<DeliveryMethod> deliveryMethodRepository,
+            IGenericRepository<Product> productRepository, IBasketRepository basketRepository)
+        {
+            this._ = orderRepository;
+            this._deliveryMethodRepository = deliveryMethodRepository;
+            this._productRepository = productRepository;
+            this._basketRepository = basketRepository;
+        }
+
+        public async Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethodId, string basketId, Address shippingAddress)
+        {
+            var basket = await _basketRepository.GetBasketAsync(basketId);
+            var items = new List<OrderItem>();
+
+            foreach (var item in basket.Items)
+            {
+                var productItem = await _productRepository.GetByIdAsync(item.Id);
+                var itemOrdered = new ProductItemOrdered(productItem.Id, productItem.Name, productItem.PictureUrl);
+                var orderItem = new OrderItem(itemOrdered, productItem.Price, item.Quantity);
+                items.Add(orderItem);
+            }
+
+            var deliveryMethod = await _deliveryMethodRepository.GetByIdAsync(deliveryMethodId);
+
+            var subtotal = items.Sum(item => item.Price * item.Quantity);
+
+            var order = new Order(items, buyerEmail, shippingAddress, deliveryMethod, subtotal);
+
+            return order;
+        }
+
+        public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetOrderByIdAsync(int id, string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

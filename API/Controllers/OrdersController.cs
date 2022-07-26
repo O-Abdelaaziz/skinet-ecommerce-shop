4d4dfb0cd82ea95aea319e0ba2df64080d50503f
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-26 22:40:16 +0100

    Section(api orders) - 154: creating the order controller

---
+++ API/Controllers/OrdersController.cs
@@ -0,0 +1,41 @@
+﻿using API.Dtos;
+using API.Errors;
+using AutoMapper;
+using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class OrdersController : BaseApiController
+    {
+        private readonly IOrderService _orderService;
+        private readonly IMapper _mapper;
+
+        public OrdersController(IOrderService orderService, IMapper mapper)
+        {
+            _mapper = mapper;
+            _orderService = orderService;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Order>> CreateOrder(OrderDto orderDto)
+        {
+            var email = HttpContext.User.FindFirstValue(ClaimTypes.Email);
+
+            var address = _mapper.Map<AddressDto, Address>(orderDto.ShipToAddress);
+
+            var order = await _orderService.CreateOrderAsync(email, orderDto.DeliveryMethodId, orderDto.BasketId, address);
+
+            if (order == null) return BadRequest(new ApiResponse(400, "Problem creating order"));
+
+            return Ok(order);
+        }
+    }
+}

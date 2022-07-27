fabc305128aed7f8b7b4b799a90851c4f0f23e0b
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-07-27 21:18:44 +0100

    Section(client checkout) - 168: creating the checkout components

--- client/src/app/checkout/checkout.module.ts
+++ client/src/app/checkout/checkout.module.ts
@@ -4,11 +4,21 @@ import { CommonModule } from '@angular/common';
 import { CheckoutRoutingModule } from './checkout-routing.module';
 import { CheckoutComponent } from './checkout.component';
 import {SharedModule} from "../shared/shared.module";
+import { CheckoutAddressComponent } from './components/checkout-address/checkout-address.component';
+import { CheckoutDeliveryComponent } from './components/checkout-delivery/checkout-delivery.component';
+import { CheckoutReviewComponent } from './components/checkout-review/checkout-review.component';
+import { CheckoutPaymentComponent } from './components/checkout-payment/checkout-payment.component';
+import { CheckoutSuccessComponent } from './components/checkout-success/checkout-success.component';
 
 
 @NgModule({
   declarations: [
-    CheckoutComponent
+    CheckoutComponent,
+    CheckoutAddressComponent,
+    CheckoutDeliveryComponent,
+    CheckoutReviewComponent,
+    CheckoutPaymentComponent,
+    CheckoutSuccessComponent
   ],
   imports: [
     CommonModule,

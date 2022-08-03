46d6e88a405f6b05ed90541604fa1d82bc765e98
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-08-03 19:49:41 +0100

    Section(taking payments) - 193: adding stripe element to the components

--- client/src/app/checkout/components/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/components/checkout-payment/checkout-payment.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FormGroup} from "@angular/forms";
 import {BasketService} from "../../../basket/services/basket.service";
 import {CheckoutService} from "../../services/checkout.service";
@@ -6,19 +6,55 @@ import {AngularNotifierService, NotifierEnum} from "../../../core/services/angul
 import {IBasket} from "../../../shared/models/basket";
 import {NavigationExtras, Router} from "@angular/router";
 
+declare var Stripe:any;
+
 @Component({
   selector: 'app-checkout-payment',
   templateUrl: './checkout-payment.component.html',
   styleUrls: ['./checkout-payment.component.scss']
 })
-export class CheckoutPaymentComponent implements OnInit {
+export class CheckoutPaymentComponent implements OnInit, OnDestroy {
   @Input()
   public checkoutForm!: FormGroup;
+  @ViewChild('cardNumber', {static: true})
+  public cardNumberElement: ElementRef = {} as ElementRef;
+  @ViewChild('cardExpiry', {static: true})
+  public cardExpiryElement: ElementRef = {} as ElementRef;
+  @ViewChild('cardCvc', {static: true})
+  public cardCvcElement: ElementRef = {} as ElementRef;
+  public stripe: any;
+  public cardNumber: any;
+  public cardExpiry: any;
+  public cardCvc: any;
+  public cardErrors: any;
 
   constructor(private _basketService: BasketService, private _checkoutService: CheckoutService, private _angularNotifierService: AngularNotifierService, private _router: Router) {
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy() {
+    this.cardNumber.destroy();
+    this.cardExpiry.destroy();
+    this.cardCvc.destroy();
+  }
+
+  ngAfterViewInit() {
+    this.stripe = Stripe('pk_test_51LS4hsEBaeITZMNNGnEv4O8jgvEyOjlPeXsED0vh1AsfOQaWYQZaFIid8qpJS14T2nitkaPvu9LVmgB2BbcN5euy00lI10BQqE');
+    const elements = this.stripe.elements();
+
+    this.cardNumber = elements.create('cardNumber');
+    this.cardNumber.mount(this.cardNumberElement.nativeElement);
+    //this.cardNumber.addEventListener('change', this.cardHandler);
+
+    this.cardExpiry = elements.create('cardExpiry');
+    this.cardExpiry.mount(this.cardExpiryElement.nativeElement);
+    //this.cardExpiry.addEventListener('change', this.cardHandler);
+
+    this.cardCvc = elements.create('cardCvc');
+    this.cardCvc.mount(this.cardCvcElement.nativeElement);
+    //this.cardCvc.addEventListener('change', this.cardHandler);
   }
 
   onSubmitOrder() {

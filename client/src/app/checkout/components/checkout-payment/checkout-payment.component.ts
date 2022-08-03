dea4c8fc9dda0e2b8503482de9dda923104f5abc
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-08-03 20:25:36 +0100

    Section(taking payments) - 194: displaying card validation errors

--- client/src/app/checkout/components/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/components/checkout-payment/checkout-payment.component.ts
@@ -27,6 +27,11 @@ export class CheckoutPaymentComponent implements OnInit, OnDestroy {
   public cardExpiry: any;
   public cardCvc: any;
   public cardErrors: any;
+  public cardHandler = this.onChange.bind(this);
+  public loading = false;
+  public cardNumberValid = false;
+  public cardExpiryValid = false;
+  public cardCvcValid = false;
 
   constructor(private _basketService: BasketService, private _checkoutService: CheckoutService, private _angularNotifierService: AngularNotifierService, private _router: Router) {
   }
@@ -40,21 +45,41 @@ export class CheckoutPaymentComponent implements OnInit, OnDestroy {
     this.cardCvc.destroy();
   }
 
+  onChange(event:any) {
+    if (event.error) {
+      this.cardErrors = event.error.message;
+    } else {
+      this.cardErrors = null;
+    }
+    switch (event.elementType) {
+      case 'cardNumber':
+        this.cardNumberValid = event.complete;
+        break;
+      case 'cardExpiry':
+        this.cardExpiryValid = event.complete;
+        break;
+      case 'cardCvc':
+        this.cardCvcValid = event.complete;
+        break;
+    }
+  }
+
+
   ngAfterViewInit() {
     this.stripe = Stripe('pk_test_51LS4hsEBaeITZMNNGnEv4O8jgvEyOjlPeXsED0vh1AsfOQaWYQZaFIid8qpJS14T2nitkaPvu9LVmgB2BbcN5euy00lI10BQqE');
     const elements = this.stripe.elements();
 
     this.cardNumber = elements.create('cardNumber');
     this.cardNumber.mount(this.cardNumberElement.nativeElement);
-    //this.cardNumber.addEventListener('change', this.cardHandler);
+    this.cardNumber.addEventListener('change', this.cardHandler);
 
     this.cardExpiry = elements.create('cardExpiry');
     this.cardExpiry.mount(this.cardExpiryElement.nativeElement);
-    //this.cardExpiry.addEventListener('change', this.cardHandler);
+    this.cardExpiry.addEventListener('change', this.cardHandler);
 
     this.cardCvc = elements.create('cardCvc');
     this.cardCvc.mount(this.cardCvcElement.nativeElement);
-    //this.cardCvc.addEventListener('change', this.cardHandler);
+    this.cardCvc.addEventListener('change', this.cardHandler);
   }
 
   onSubmitOrder() {
